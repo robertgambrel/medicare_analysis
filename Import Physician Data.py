ff13073330c35bfe9d4c708e70b59305550d3435
Robert Gambrel <robert.gambrel@gmail.com>
2016-07-07 18:04:28 -0500

    Add prescriptions, initial subset to opiates

--- Import Physician Data.py
+++ Import Physician Data.py
@@ -5,11 +5,20 @@ Import tab-delineated Medicare data
 
 import pandas as pd
 import numpy as np
+import os
+import re
 
-docs = pd.read_table('Data/Medicare_Provider_Util_Payment_PUF_CY2014'
-                     '/Medicare_Provider_Util_Payment_PUF_CY2014.txt',
-                     skiprows = 3,
-                     nrows = 100)
+if os.name == 'nt':
+    docs = pd.read_table('.\\medicare_analysis\\Data\\Medicare_Provider_Util_Payment_PUF_CY2014.txt',
+                         skiprows=3)
+    scripts = pd.read_table('.\\medicare_analysis\\Data\\PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab',
+                         skiprows=1)
+else:
+    docs = pd.read_table('Data/Medicare_Provider_Util_Payment_PUF_CY2014.txt',
+                         skiprows = 3)
+
+
+
 
 docs.columns = ['npi',
                 'nppes_provider_last_org_name',
@@ -37,3 +46,35 @@ docs.columns = ['npi',
                 'average_submitted_chrg_amt',
                 'average_Medicare_payment_amt',
                 'average_Medicare_standard_amt']
+
+scripts.columns = ['npi',
+                   'nppes_provider_last_org_name',
+                   'nppes_provider_first_name',
+                   'nppes_provider_city',
+                   'nppes_provider_state',
+                   'specialty_description',
+                   'description_flag',
+                   'drug_name',
+                   'generic_name',
+                   'bene_count',
+                   'total_claim_count',
+                   'total_day_supply',
+                   'total_drug_cost',
+                   'bene_count_ge65',
+                   'bene_count_ge65_redact_flag',
+                   'total_claim_count_ge65',
+                   'ge65_redact_flag',
+                   'total_day_supply_ge65',
+                   'total_drug_cost_ge65']
+
+any(re.match("OXYCODONE|CODEINE|FENTANYL|HYDROCODONE|HYDROMORPHONE|MEPERIDINE|METHADONE|MORPHINE", x)
+    for x in scripts['generic_name'].head(10000))
+
+
+opiate_matches = scripts.generic_name.str.contains("OXYCODONE|CODEINE|FENTANYL|HYDROCODONE|HYDROMORPHONE|MEPERIDINE|METHADONE|MORPHINE", regex = True,
+                                                   na = False)
+
+opiate_scripts = scripts[opiate_matches]
+
+
+
